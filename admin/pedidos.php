3519d1f1750b9c1d87db47070d25934547b53e53
YuriLima1 <yurialexandresilvalima@gmail.com>
2024-10-17 00:25:57 -0300

    começando os pedidos

---
+++ admin/pedidos.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pedido | Agro Malandrin</title>
+</head>
+
+    <?php
+        include_once 'includes/header.php';
+        require_once 'includes/dbconnect.php';
+    ?>
+<body>
+    <div class="container justify-content-center">
+        <div class="row"> 
+            <div class="col-md-6 mx-auto"> 
+
+                <div class="box"> 
+                    
+                    <h3>Cadastro de Pedidos</h3><br><br> 
+                    
+                        <form action="" method="POST">
+                            
+                            <input type="hidden" name="id_pedd" value="">
+
+                            <input type="hidden" name="data_pedd" value="">
+
+                            <label for=""></label>
+
+
+</body>
+</html>
