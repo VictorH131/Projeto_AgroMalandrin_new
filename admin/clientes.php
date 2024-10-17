fe6a080547695ff16948ba2519fab66f6198a33a
YuriLima1 <yurialexandresilvalima@gmail.com>
2024-10-17 00:19:21 -0300

    add um botao e paramos na tbl de cli atv e n atv

--- admin/clientes.php
+++ admin/clientes.php
@@ -9,8 +9,13 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Cliente | Agro Malandrin</title>
-
     </head>
+
+    <?php
+    include_once 'includes/header.php';
+    require_once 'includes/dbconnect.php';
+    ?>
+
 <body>
 
 <div class="container">
@@ -21,16 +26,16 @@
 
                     <h3><i class="glyphicon glyphicon-plus"></i>&nbsp;Tabela de Clientes</h3><br><br>
 
-                        <form action="" method="post">
+                        <form action="" method="POST">
 
                             <input type="hidden" name="id_cliente" value="">
 
-                            <label for="nome">Nome do Cliente:</label><br>
-                            <input type="text" name="nome" value="" class="form-control"><br><br>
+                            <label for="nome_cli">Nome do Cliente:</label><br>
+                            <input type="text" name="nome_cli" value="" class="form-control"><br><br>
 
-                            <label for="tipo_do_documento">Tipo do Documento:</label><br>
-                                <select name="tipo_do_documento" required class="form-control">
-                                    <div class="tipo_documento">
+                            <label for="tipo_do_documento_cli">Tipo do Documento:</label><br>
+                                <select name="tipo_do_documento_cli" required class="form-control">
+                                    <div class="tipo_documento_cli">
                                         <optgroup label="Documento">
                                             <option value="invalido">Selecione</option>
                                             <option value="cpf">CPF</option>
@@ -40,14 +45,14 @@
                                     </div>
                                 </select><br>
 
-                            <label for="documento_cliente">Documento:</label><br>
-                            <input type="text" name="documento_cliente" value="" required class="form-control"> <br><br>
+                            <label for="documento_cli">Documento:</label><br>
+                            <input type="text" name="documento_cli" value="" required class="form-control"> <br><br>
 
                             <label for="data_nascimento">Data de Nascimento:</label><br>
                             <input type="date" name="data_nascimento" value="" required class="form-control"><br><br>
 
-                            <label for="email_cliente">Email:</label><br>
-                            <input type="email" name="email_cliente" value="" required class="form-control"><br><br>
+                            <label for="email_cli">Email:</label><br>
+                            <input type="email" name="email_cli" value="" required class="form-control"><br><br>
 
                             <label for="cep">CEP:</label><br>
                             <input type="text" name="cep" value="" required class="form-control"><br><br>
@@ -61,8 +66,8 @@
                             <label for="cidade">Cidade:</label><br>
                             <input type="text" name="cidade" value="" required class="form-control"><br><br>
 
-                            <label for="telefone_cliente">Telefone:</label><br>
-                            <input type="text" name="telefone_cliente" value="" required class="form-control"><br><br>
+                            <label for="telefone_cli">Telefone:</label><br>
+                            <input type="text" name="telefone_cli" value="" required class="form-control"><br><br>
 
                             <label for="uf">UF:</label><br>
                             <select name="uf" required class="form-control"><br>
@@ -70,11 +75,12 @@
                                 <?php
 
                                 ?>
-                            </select>
+                            </select><br><br>
             
-                            <br><br>
+                            <button type="submit"> </button>
+                        </form><br><br>
 
-                        </form>
+                        <h3>Tabela de Clientes Ativos/Não Ativos</h3>
                 </div>
             </div>
         </div>
